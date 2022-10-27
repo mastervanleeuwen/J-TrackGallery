b5b6acd29622cd4e1c886a7f215709062e370f42
Marco van Leeuwen <mastervanleeuwen@gmail.com>
2022-10-27 18:23:18 +0200

    Test fix for SQL foreign key syntax

--- administrator/components/com_jtg/sql/install.sql
+++ administrator/components/com_jtg/sql/install.sql
@@ -99,13 +99,13 @@ CREATE TABLE IF NOT EXISTS `#__jtg_files` (
 
 CREATE TABLE IF NOT EXISTS `#__jtg_photos` (
 	`id` int(10) NOT NULL AUTO_INCREMENT,
-        `trackID` int(10) NOT NULL,
+	`trackID` int(10) NOT NULL,
 	`title` varchar(256),
 	`filename` varchar(50) NOT NULL,
 	`lat` float(24),
-        `lon` float(24), 
-	PRIMARY KEY (`id`),
-        FOREIGN KEY (`trackID`) REFERENCES `#__jtg_files(id)`
+	`lon` float(24), 
+	PRIMARY KEY (id),
+	FOREIGN KEY (trackID) REFERENCES #__jtg_files (id)
 ) ENGINE=MyISAM	DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `#__jtg_votes` (
