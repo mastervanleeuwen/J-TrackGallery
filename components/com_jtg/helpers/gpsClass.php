0750de769a16a7e2472516825b46e957df6fc1ec
Marco van Leeuwen <mastervanleeuwen@gmail.com>
2022-12-20 10:40:39 +0100

    More debug printouts

--- components/com_jtg/helpers/gpsClass.php
+++ components/com_jtg/helpers/gpsClass.php
@@ -181,9 +181,10 @@ class GpsDataClass
 			$this->start = $this->route[0]->coords[0];
 	
 		error_log("gpsClass: check whether we have time on the first point: ".$this->start[3]);
-		$this->speedDataExists = ( ( isset ($this->start[3])  && $this->start[3] > 0) ? true: false);
+		echo "gpsClass: check whether we have time on the first point: ".$this->start[3]." ".((isset($this->start[3])&&$this->start[3]>0)?"true\n":"false\n");
+		$this->speedDataExists = ( (isset($this->start[3])  && !empty($this->start[3])) ? true: false);
 		$this->elevationDataExists = ( isset ($this->start[2])? true: false);
-		$this->beatDataExists = ( (isset ($this->start[4]) && $this->start[4] > 0)? true: false);
+		$this->beatDataExists = ( (isset($this->start[4]) && $this->start[4] > 0)? true: false);
 
 		// Calculate allCoords, distance, elevation max lon...
 		$this->extractAllTracksCoords();
