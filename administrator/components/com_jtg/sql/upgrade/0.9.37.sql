1bdbe997a5a1ff22533aa3bb7dcfde54295e595f
Marco van Leeuwen <mastervanleeuwen@gmail.com>
2023-07-31 21:41:34 +0200

    Change/improve SQL syntax for 0.9.37 update

--- administrator/components/com_jtg/sql/upgrade/0.9.37.sql
+++ administrator/components/com_jtg/sql/upgrade/0.9.37.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `#__jtg_files`
-ADD `icon_n` FLOAT(20) NOT NULL DEFAULT start_n,
-ADD `icon_e` FLOAT(20) NOT NULL DEFAULT start_e;
+ADD `icon_n` FLOAT(20),
+ADD `icon_e` FLOAT(20);
+UPDATE `#__jtg_files`
+SET icon_n = start_n, icon_e = start_e;
