15921190ceca6f8b0c0bf9171aefd069ef171153
Marco van Leeuwen <mastervanleeuwen@gmail.com>
2023-08-17 22:15:52 +0200

    Add protection against exist existing columns in upgrade script

--- administrator/components/com_jtg/sql/upgrade/0.9.37.sql
+++ administrator/components/com_jtg/sql/upgrade/0.9.37.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `#__jtg_files`
-ADD `icon_n` FLOAT(20),
-ADD `icon_e` FLOAT(20);
+ADD IF NOT EXISTS `icon_n` FLOAT(20),
+ADD IF NOT EXISTS `icon_e` FLOAT(20);
 UPDATE `#__jtg_files`
 SET icon_n = start_n, icon_e = start_e;
